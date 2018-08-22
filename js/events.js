07096bb663c96c8cf4b93ed6768fc4be8a8a97f0
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-22 17:41:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key){
+    if (key === 71) {
+      alert('YOU PRESSED G KEY!')
+    }
+  }
+)}
 
 $(document).ready(function(){
 
 // call functions here
+//getIt()
 
 });
